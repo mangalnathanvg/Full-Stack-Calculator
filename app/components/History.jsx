982950aac3a4abf41af44f458f7219076f47481a
Mangalnathan Vijayagopal <Mangalnathan.Vijayagopal@cerner.com>
2018-02-28 13:52:27 +0530

    Updated components

---
+++ app/components/History.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var createReactClass = require('create-react-class');
+
+const contentStyle = {
+    color:'black'
+}
+module.exports = createReactClass({
+    render:function(){
+        return (
+            <h1 style={contentStyle}>The character later appears in person when sending a messenger to boast his renewed intentions of conquering his universe before his aging adversary. In response, Doctor Strange travels to Dormammu's "Dark Dimension" as the Ancient One does not consider himself powerful enough to defeat Dormammu, and manages to overcome all supernatural servitors sent against him. Dormammu engages Strange in mystic combat and shows himself to be far more powerful, but, when drawing upon the realm's energies, inadvertently weakens the barrier containing the horde of Mindless Ones. Since they threatened Dormammu's subjects, Strange helped his enemy to re-seal the juggernauts by letting power from his amulet flow into Dormammu. Indebted to Strange for his help, Dormammu ends the fight, and in return Strange demands a binding vow to never enter the "Earth realm" again and to not harm Clea. Dormammu complies, but gains a burning hatred against Strange for the humiliation.</h1>
+
+             );
+    }
+});
--- app/main.jsx
+++ app/main.jsx
@@ -3,5 +3,7 @@ var ReactDOM = require('react-dom');
 console.log("Hello from JSX!!");
 
 var Calc = require('./components/Calc.jsx');
+var History = require('./components/History.jsx');
 
 ReactDOM.render(<Calc />, app)
+ReactDOM.render(<History />, log)
